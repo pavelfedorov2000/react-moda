9800ba810decfd0e956b16b274e05ba00412e186
Pavel Fedorov <p.fedorov@imedia.by>
2023-02-14 19:03:49 +0300

    global refactoring

--- src/Components/Profile/Subscribes/Subscribes.tsx
+++ src/Components/Profile/Subscribes/Subscribes.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useActions } from '../../../hooks/useActions';
 import { useTypedSelector } from '../../../hooks/useTypedSelector';
 import Button from '../../Button';
