4cf5b6cfa15722c56df26386b402976217a8e084
Pavel <66357864+pavelfedorov2000@users.noreply.github.com>
2025-08-29 12:58:31 +0300

    remove inner key

--- src/Components/CatalogDropSort.tsx
+++ src/Components/CatalogDropSort.tsx
@@ -8,7 +8,7 @@ const CatalogDropSort = ({ name, type, order }: Sort) => {
     const { setSortBy } = useActions();
 
     return (
-        <label key={type} className="catalog-drop-filter__item radio">
+        <label className="catalog-drop-filter__item radio">
             <input onChange={() => setSortBy({ type, order })} className="radio-box" type="radio" name="SORT" checked={sortBy.type === type && sortBy.order === order} />
                 <span className="radio-style">
                     <span style={{ backgroundImage: `url(${checkIcon})` }}></span>
