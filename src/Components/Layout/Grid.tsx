e569159809a71c2908c298ff4d64e0922fedb259
Pavel <66357864+pavelfedorov2000@users.noreply.github.com>
2025-09-02 22:38:40 +0300

    Update Grid.tsx

--- src/Components/Layout/Grid.tsx
+++ src/Components/Layout/Grid.tsx
@@ -15,26 +15,26 @@ interface Props {
     isUrlSplice?: boolean;
 }
 
+function checkIfTwoColumnsItem(items: any[], listItems: any[], index: number) {
+    return items?.breakpoint === 'xl'
+            && items?.value === 4
+            && listItems.length === items?.value - 1
+            && index === 0;
+}
+
 const mainClass = ClassName.Grid;
 
 const Grid = ({ className, items, listItems, listItem, ...props }: Props & WithClassName) => {
     const RenderedItem = listItem;
-
-    const checkTwoColumnsItem = useCallback((index: number) => {
-        return items?.breakpoint === 'xl'
-            && items?.value === 4
-            && listItems.length === items?.value - 1
-            && index === 0;
-    }, [items, listItems]);
     
     return (
         <div className={classNames(mainClass, className, {
             [`${mainClass}--items-${items?.breakpoint}_${items?.value}`]: items
         })}>
-            {listItems.map((item, index) => (
+            {listItems?.map((item, index) => (
                 <RenderedItem key={item.id ?? index} className={classNames({
                     [`${className}__item`]: className,
-                    'span-2': checkTwoColumnsItem(index)
+                    'span-2': checkIfTwoColumnsItem(items, listItems, index)
                 })} {...item} {...props} />
             ))}
         </div>
